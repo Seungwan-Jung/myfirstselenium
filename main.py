c480cc7485345e9a79af403243e1294be1aedf1d
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-04-25 23:41:49 +0900

    1.4 waiting for elements

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 # 1. pip install selenium
 # 2. pip install webdriver_manager
@@ -18,13 +21,17 @@ search_bar = browser.find_element_by_class_name("gLFyf")
 search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
-search_results = browser.find_elements_by_class_name("g")
+shitty_element = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
+)
+print(shitty_element)
+# search_results = browser.find_elements_by_class_name("g")
 
-for index, search_result in enumerate(search_results):
-    class_name = search_result.get_attribute("class")
-    if "kno-kp" not in class_name:
-        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+# for index, search_result in enumerate(search_results):
+#     class_name = search_result.get_attribute("class")
+#     if "kno-kp" not in class_name:
+#         search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
 
-browser.quit()
+# browser.quit()
 
 # browser.quit()
