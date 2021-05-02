bb28dc185c50b3622a421a90b6f238c9cbd6dedf
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-05-03 01:45:52 +0900

    3.1 using tabs

--- instamining.py
+++ instamining.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -19,7 +20,12 @@ header = WebDriverWait(browser, 10).until(
 hashtags = header.find_elements_by_class_name("AC7dP")
 
 for hashtag in hashtags:
-    hashtag.click()
+    ActionChains(browser).key_down(Keys.COMMAND).click(hashtag).perform()
+
+for window in browser.window_handles:
+    browser.switch_to.window(window)
+    hashtag_name = browser.find_element_by_tag_name("h1")
+    print(hashtag_name.text[1:])
 
 time.sleep(3)
 browser.quit()
